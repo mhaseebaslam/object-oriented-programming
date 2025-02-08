7777ffc42a2ef2eb11b7973c2fd6dbe43f56927e
Muhammad Haseeb <105885993+mhaseebaslam@users.noreply.github.com>
2025-02-08 23:55:49 +0200

    solution for exercise-1-create-belgium-object

---
+++ js-object-literals/exercise-1-create-belgium-object.js
@@ -0,0 +1,21 @@
+/* Belgium is an object with the following properties:
+- country: "Belgium"
+- capital: "Brussels"
+- languages: ["Dutch", "French", "German"]
+- area: "30689 km²"
+- population: 11492641
+- gdpPerCapita: "$50114"
+- iso3166Code: "BE" */
+
+const belgium = {
+  country: "Belgium",
+  capital: "Brussels",
+  languages: ["Dutch", "French", "German"],
+  area: "30689 km²",
+  population: 11492641,
+  gdpPerCapita: "$50114",
+  iso3166Code: "BE",
+};
+
+// Output
+console.log(belgium);
