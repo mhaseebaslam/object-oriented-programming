ef63f0dfb77dffab6bffd5ae4c8be61991331c9d
Muhammad Haseeb <105885993+mhaseebaslam@users.noreply.github.com>
2025-02-08 02:13:13 +0200

    solution for exercise-1-basic-average-calculation

---
+++ oop-browser-exercises/exercise-1-basic-average-calculation.js
@@ -0,0 +1,18 @@
+// Declare variables to store the numbers
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+let num4 = 40;
+let num5 = 50;
+
+// Declare a variable to store the sum of the numbers
+let sum = num1 + num2 + num3 + num4 + num5;
+
+// Declare a variable to store the count of numbers
+let count = 5;
+
+// Calculate the average
+let average = sum / count;
+
+// Display the result to the user
+console.log("The average of the five numbers is: " + average);
